0ebbd2cdd76e69ea15b345c5f519fadad85f6798
aviratpatel <avirat.patel@gmail.com>
2020-03-08 17:45:27 +0530

    Second commit;

--- src/main/java/com/stacksimplify/restservices/Application.java
+++ src/main/java/com/stacksimplify/restservices/Application.java
@@ -3,6 +3,7 @@ package com.stacksimplify.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// No controller class created yet;
 @SpringBootApplication
 public class Application {
 
